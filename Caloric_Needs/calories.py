8cb191ee44d0b41f209cc039c6c139c1861f51e0
SchillShark <30481740+SchillShark@users.noreply.github.com>
2023-04-25 11:08:04 -0500

    Update

--- Caloric_Needs/calories.py
+++ Caloric_Needs/calories.py
@@ -13,24 +13,24 @@ multiply_weight_by = 0
 while True:
 	# Check users input is correct and within scope
 	print("\nPlease enter 1 , 2 or 3")
-	user_fitness_level = raw_input("\n: ")
+	user_fitness_level = input("\n: ")
 
 	if user_fitness_level == "1":
 		multiply_weight_by = 12
-               	break
-        elif user_fitness_level == "2":
-               	multiply_weight_by = 13
-               	break
-        elif user_fitness_level == "3":
-               	multiply_weight_by = 14
-               	break
-        else:
-               	print("\nError! Please enter the numbers 1 , 2  or 3")
+		break
+	elif user_fitness_level == "2":
+		multiply_weight_by = 13
+		break
+	elif user_fitness_level == "3":
+		multiply_weight_by = 14
+		break
+	else:
+		print("\nError! Please enter the numbers 1 , 2  or 3")
 		continue
 print("Loading....")
 
 # Get users weight
-user_weight = int(raw_input("Please enter your current weight (Examples: 150, 174, 223)\n: "))
+user_weight = int(input("Please enter your current weight (Examples: 150, 174, 223)\n: "))
 
 # Maintenence Caloric needs 
 maintenence_caloric_needs = round(user_weight * multiply_weight_by, -2)
@@ -44,13 +44,13 @@ insanity_caloric_needs = maintenence_caloric_needs + 600
 while True: 
 	print("1 = Weight Loss\n2 = Weight Maintenance\n3 = Weight Gain\n")
 	print("Please enter 1 , 2 or 3 for your final caloric needs for the Insanity Workout Program!")
-	user_goals = raw_input(": ") 
+	user_goals = input(": ") 
 	if user_goals == "1":
 		insanity_caloric_needs = insanity_caloric_needs - 500
 		insanity_caloric_needs = round(insanity_caloric_needs, -2)
 		break
 	elif user_goals == "2": 
-                insanity_caloric_needs = round(insanity_caloric_needs, -2)
+		insanity_caloric_needs = round(insanity_caloric_needs, -2)
 		break
 	elif user_goals == "3": 
 		insanity_caloric_needs = insanity_caloric_needs + 500
---
+++ meal_generator.py
@@ -0,0 +1,34 @@
+# This application will give the user his/her meals for the week 
+import random
+
+# List of meal titles
+meal1 = ["Pro-Oatmeal", "Fruit and Cottage Cheese", "Berry Protein Smoothie", "Bagel and Lox", "Egg White and Fruit Plate", "Egg White Breakfast Wrap", "Bowl of Cereal", "Peanut Butter Toast", "Veggie Omlet"]
+meal2 = ["Mexican Style Eggs", "Protein Pancakes", "Warm Cereal Bowl", "Egg Sandwhich", "Protein Omlet", "Vanilla-Berry Protein Shake", "Yogurt Bowl", "Nutrition Bar", "Deli Sandwich", "Turkey BLT"]
+meal3 = ["Grilled Chicken Salad", "Sushi Roll", "Black Bean Soup and Half Sandwich", "Roast Beef Sandwich", "Teriyaki Grilled Tuna", "Chicken Ranch Wrap", "Lean Burger", "Whole Wheat Past With Vegtables and Feta", "Salmon Nicoise Plate"]
+meal4 = ["Sashimi", "Cold Cut Platter", "Peanut Butter and Jelly", "Tuna Salad In A Tomato", "Turkey Lettuce Wrap and Bean Salad", "Turkey Chili", "Protein Pizza Muffin", "Shrimp Cocktail Platter", "Rotisserie Chicken And Salad", "Roast Beef Wrap"]
+meal5 = ["Baked Cod With Steamed Carrots, Corn, and Cauliflower", "Dinner Omlet", "Steak With Broccoli", "Chicken Meatballs", "Grilled Salmon With Asparagus", "Citrus Baked Chicken With Glazed Carrots", "Brown Rice Bowl", "Turkey Burger", "Chicken Stir Fry With Broccoli, Mushrooms, And Snow Peas", "Pasta With Seafood Marinara"]
+
+def meals_for_the_week():
+# Print the meals randomly
+    for meal in meal1:
+        r = random.randint(0, len(meal1)-1)
+        print("Meal 1 is: " + meal1[r] )
+        break
+    for meal in meal2:
+        r = random.randint(0, len(meal2)-1)
+        print("Meal 2 is: " + meal2[r] )
+        break
+    for meal in meal3:
+        r = random.randint(0, len(meal3)-1)
+        print("Meal 3 is: " + meal3[r] )
+        break
+    for meal in meal4:
+        r = random.randint(0, len(meal4)-1)
+        print("Meal 4 is: " + meal4[r] )
+        break
+    for meal in meal5:
+        r = random.randint(0, len(meal5)-1)
+        print("Meal 5 is: " + meal5[r] )
+        break
+
+meals_for_the_week()
